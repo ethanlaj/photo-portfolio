54b7abaaac63d9f60aad8c2774d7819f21fbc55a
Ethan Lajeunesse <ethan@lajfam.com>
2023-07-29 15:01:17 -0400

    Revert "Compress images"
    This reverts commit 16ace62528459374e59d7f4c9c49632a28c5ecdf.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      mozjpeg: {
-        quality: 20,
-      },
-    }),
-  ],
+  plugins: [react()],
 })
