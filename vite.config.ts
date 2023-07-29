16ace62528459374e59d7f4c9c49632a28c5ecdf
Ethan Lajeunesse <ethan@lajfam.com>
2023-07-29 14:25:10 -0400

    Compress images

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      mozjpeg: {
+        quality: 20,
+      },
+    }),
+  ],
 })
